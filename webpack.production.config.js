6005a3e0e54c00f6df4ce335a3608a0a4b11c376
AlexanderParshin <alexparshin20150@gmail.com>
2021-02-24 13:35:09 +0300

    Fixed bug: changed entry point in webpack

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -28,7 +28,7 @@ const config = {
   },
   entry: {
     main: './main.js',
-    root: './config/root.js'
+    root: './root.js'
   }, // [('./main.js', './config/root.js')],
   resolve: {
     alias: {
